774552d08760725c9e6f0d3eb647cc90a0ff75e5
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-02-23 13:00:54 -0300

    Edit function docstring

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 /// <reference types="./utils.d.ts" />
 
 /**
- * Scan the bookmarks tree based on the BFS algorithm, making useful for batch processing by setting a pre-fixed
- * `maxNodes` number.
- * @param n Number of nodes to retrieve in result
+ * Scan the bookmarks tree based on the BFS algorithm, making it useful for batch processing.
+ * @param n Number of nodes to retrieve in result, which means that `nodes` array in `BookmarksTreeScanResult` will 
+ *  contain between 0 and`maxNodes` entries.
  */
 async function scanBookmarkTree(maxNodes: number): Promise<BookmarksTreeScanResult> {
   const tree = await chrome.bookmarks.getTree()
