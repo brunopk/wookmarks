57c81acf41a524c77e5bf458ceb94d870915efcb
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-02-24 18:22:57 -0300

    Introducing MUI library

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from '/react.svg'
-import viteLogo from '/vite.svg'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { FolderOpen, Link, LinkOff } from '@mui/icons-material'
+import Accordion from '@mui/material/Accordion'
+import Badge from '@mui/material/Badge'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Typography from '@mui/material/Typography'
+import CssBaseline from '@mui/material/CssBaseline'
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon  from '@mui/material/ListItemIcon'
+import ListItemText  from '@mui/material/ListItemText'
 import './App.css'
 
 function openNewTab(setIsNewTab: React.Dispatch<React.SetStateAction<boolean>>) {
@@ -16,24 +27,113 @@ async function scanBookmarkTree() {
 }
 
 function App() {
-  const [count, setCount] = useState(0)
   const [isNewTab, setIsNewTab] = useState(false)
+  const [expanded, setExpanded] = React.useState<string | false>(false);
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <div>
+        <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+          <AccordionSummary
+            sx={{
+              display: "flex",
+              alignItems: "center", 
+              justifyContent: "space-between", 
+              "&:hover": {
+                backgroundColor: "rgba(255, 255, 255, 0.08)", 
+              },
+              transition: "background-color 0.3s"
+            }}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+            onMouseDown={(event) => event.preventDefault()} // Prevents focus
+          >
+            <FolderOpen sx={{marginRight: '1rem'}}/>
+            <Typography component="span" sx={{ display: "flex", alignItems: "flex-end", flexGrow: 1 }}>
+              Folder 1
+            </Typography>
+            <Badge badgeContent={4} color="error" sx={{marginRight: '1rem'}}>
+              <LinkOff color="action" />
+            </Badge>
+          </AccordionSummary>
+          <AccordionDetails>
+            <List
+              sx={{ width: '100%'}}
+              component="nav"
+              aria-labelledby="nested-list-subheader"
+            >
+              <ListItemButton>
+                <ListItemIcon>
+                  <FolderOpen />
+                </ListItemIcon>
+                <ListItemText primary="Sent mail" />
+              </ListItemButton>
+              <ListItemButton>
+                  <ListItemIcon>
+                    <Link />
+                  </ListItemIcon>
+                  <ListItemText primary="Drafts" />
+              </ListItemButton>
+            </List>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+          <AccordionSummary
+            aria-controls="panel2bh-content"
+            id="panel2bh-header"
+            onMouseDown={(event) => event.preventDefault()} // Prevents focus
+          >
+            <FolderOpen sx={{marginRight: '1rem'}}/>
+            <Typography component="span" sx={{ width: '33%', flexShrink: 0 }}>
+              Folder 2
+            </Typography>
+            <Typography component="span" sx={{ color: 'text.secondary' }}>
+              You are currently not an owner
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Donec placerat, lectus sed mattis semper, neque lectus feugiat lectus,
+              varius pulvinar diam eros in elit. Pellentesque convallis laoreet
+              laoreet.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+          <AccordionSummary
+            aria-controls="panel3bh-content"
+            id="panel3bh-header"
+            onMouseDown={(event) => event.preventDefault()} // Prevents focus
+          >
+            <FolderOpen sx={{marginRight: '1rem'}}/>
+            <Typography component="span" sx={{ width: '33%', flexShrink: 0 }}>
+              Folder 3
+            </Typography>
+            <Typography component="span" sx={{ color: 'text.secondary' }}>
+              Filtering has been entirely disabled for whole web server
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
+              amet egestas eros, vitae egestas augue. Duis vel est augue.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         {!isNewTab ? (
         <button onClick={() => openNewTab(setIsNewTab)}>
           Open new tab
@@ -42,14 +142,8 @@ function App() {
         <button onClick={() => scanBookmarkTree()}>
           Scan bookmarks
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </ThemeProvider>
   )
 }
 
