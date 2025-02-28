939f39e905d5b8ec9fcd47e98406f689e185d04b
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-02-27 22:25:46 -0300

    Change folder indicators from badges to chips

--- src/components/pages/bookmarks/Folder.tsx
+++ src/components/pages/bookmarks/Folder.tsx
@@ -1,20 +1,18 @@
-import {ReactNode} from 'react'
-import { useState } from 'react'
-import { FolderOpen, LinkOff } from '@mui/icons-material'
+import { FolderOpen } from '@mui/icons-material'
 import Accordion from '@mui/material/Accordion'
-import Badge from '@mui/material/Badge'
 import AccordionDetails from '@mui/material/AccordionDetails'
 import AccordionSummary from '@mui/material/AccordionSummary'
-import List from '@mui/material/List'
-import Typography from '@mui/material/Typography'
-import Pagination from '@mui/material/Pagination'
 import Box from '@mui/material/Box'
-import { useTheme } from '@mui/material/styles';
-
+import Chip from '@mui/material/Chip'
+import List from '@mui/material/List'
+import Pagination from '@mui/material/Pagination'
+import { useTheme } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import { ReactNode, useState } from 'react'
 
 type BookmarkFolderProps = {
-  folderName: string,
-  index: number,
+  folderName: string
+  index: number
   pageSize: number
   children: ReactNode[]
 }
@@ -22,18 +20,17 @@ type BookmarkFolderProps = {
 function BookmarkFolder({ folderName, index, pageSize, children }: BookmarkFolderProps) {
   const theme = useTheme()
 
-  const [expanded, setExpanded] = useState<string | false>(false);
+  const [expanded, setExpanded] = useState<string | false>(false)
 
-  const handleChange =
-    (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
-      setExpanded(isExpanded ? panel : false);
-    }
-  
-  const maxPages = Math.ceil(children.length/ pageSize)
+  const handleChange = (panel: string) => (_: React.SyntheticEvent, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false)
+  }
+
+  const maxPages = Math.ceil(children.length / pageSize)
   const defaultPage = 1 as number
   const [currentPage, setCurrentPage] = useState<number>(defaultPage)
   const handlePaginationChange = (_: React.ChangeEvent<unknown>, value: number) => {
-    setCurrentPage(value);
+    setCurrentPage(value)
   }
   const currentPageItems = children.slice(
     (currentPage - 1) * pageSize,
@@ -51,19 +48,34 @@ function BookmarkFolder({ folderName, index, pageSize, children }: BookmarkFolde
             backgroundColor:
               theme.palette.mode == 'dark' ? 'rgba(255, 255, 255, 0.08)' : 'rgba(0, 0, 0, 0.04)'
           },
-          transition: 'background-color 0.3s',
+          transition: 'background-color 0.3s'
         }}
         aria-controls={`panel${index}bh-content`}
         id={`panel${index}bh-header`}
         onMouseDown={(event) => event.preventDefault()} // Prevents focus
       >
-        <FolderOpen sx={{ marginRight: '1rem' }} />
-        <Typography component="span" sx={{ display: 'flex', alignItems: 'flex-end', flexGrow: 1 }}>
+        <FolderOpen sx={{ marginRight: '1rem', height: 'auto' }} />
+        <Typography component="span" sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
           {folderName}
         </Typography>
-        <Badge badgeContent={4} color="error" sx={{ marginRight: '1rem' }}>
-          <LinkOff color="action" />
-        </Badge>
+        <Chip
+          label="🔴 45"
+          sx={{ marginRight: '1rem', fontWeight: 'bold' }}
+          color="error"
+          variant="outlined"
+        />
+        <Chip
+          label="🟡 70"
+          sx={{ marginRight: '1rem', fontWeight: 'bold' }}
+          color="warning"
+          variant="outlined"
+        />
+        <Chip
+          label="🟢 80"
+          sx={{ marginRight: '1rem', fontWeight: 'bold' }}
+          color="success"
+          variant="outlined"
+        />
       </AccordionSummary>
       <AccordionDetails>
         <List component="nav" aria-labelledby="nested-list-subheader">
