cca4627e8b7141496cc1963d328b667ce6a883f6
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-03-04 20:23:03 -0300

    Start implementing modal(dialog)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
 import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import MenuIcon from '@mui/icons-material/Menu'
 import RefreshIcon from '@mui/icons-material/Refresh'
+import * as MuiMaterial from '@mui/material'
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Chip from '@mui/material/Chip'
@@ -10,10 +11,14 @@ import IconButton from '@mui/material/IconButton'
 import { createTheme, styled } from '@mui/material/styles'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import { ComponentType, ReactNode, useState } from 'react'
+import { ComponentType, Fragment, ReactNode, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
-const drawerWidth = 240
+const DRAWER_WIDTH = 240
+
+const MODAL_ROW_PADDING_IN_REM = 0.25
+
+const MODAL_WIDTH = 500
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open'
@@ -26,8 +31,8 @@ const AppBar = styled(MuiAppBar, {
     {
       props: ({ open }) => open,
       style: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: `${drawerWidth}px`,
+        width: `calc(100% - ${DRAWER_WIDTH}px)`,
+        marginLeft: `${DRAWER_WIDTH}px`,
         transition: theme.transitions.create(['margin', 'width'], {
           easing: theme.transitions.easing.easeOut,
           duration: theme.transitions.duration.enteringScreen
@@ -55,7 +60,7 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen
   }),
-  marginLeft: `-${drawerWidth}px`,
+  marginLeft: `-${DRAWER_WIDTH}px`,
   variants: [
     {
       props: ({ open }) => open,
@@ -85,8 +90,6 @@ function Layout({ Menu, children }: LayoutProps) {
     page: { title: dashboardTitle, isMenuOpen }
   } = location.state || { page: { title: '' } }
 
-  const [open, setOpen] = useState<boolean>(isMenuOpen)
-
   const theme = createTheme({
     palette: {
       mode: 'light'
@@ -107,12 +110,20 @@ function Layout({ Menu, children }: LayoutProps) {
     }
   })
 
+  const [open, setOpen] = useState<boolean>(isMenuOpen)
   const handleDrawerOpen = () => {
     setOpen(true)
   }
-
   const handleDrawerClose = () => {
     setOpen(false)
+  }
+
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+  const handleRefreshButtonClick = () => {
+    setModalOpen(true)
+  }
+  const handleModalClose = () => {
+    setModalOpen(false)
   }
 
   return (
@@ -148,25 +159,25 @@ function Layout({ Menu, children }: LayoutProps) {
           />
           <Chip
             label="🟡 70"
-            sx={{ marginRight: '1rem', fontWeight: 'bold', color: 'white'}}
+            sx={{ marginRight: '1rem', fontWeight: 'bold', color: 'white' }}
             variant="outlined"
           />
           <Chip
             label="🟢 80"
-            sx={{ marginRight: '1rem', fontWeight: 'bold', color: 'white'}}
+            sx={{ marginRight: '1rem', fontWeight: 'bold', color: 'white' }}
             variant="outlined"
           />
-          <IconButton color="inherit">
+          <IconButton color="inherit" onClick={handleRefreshButtonClick}>
             <RefreshIcon />
           </IconButton>
         </Toolbar>
       </AppBar>
       <Drawer
         sx={{
-          width: drawerWidth,
+          width: DRAWER_WIDTH,
           flexShrink: 0,
           '& .MuiDrawer-paper': {
-            width: drawerWidth,
+            width: DRAWER_WIDTH,
             boxSizing: 'border-box'
           }
         }}
@@ -185,6 +196,77 @@ function Layout({ Menu, children }: LayoutProps) {
         <DrawerHeader />
         {children}
       </Main>
+      <Fragment>
+        <MuiMaterial.Dialog
+          open={modalOpen}
+          onClose={handleModalClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <Box sx={{ width: `${MODAL_WIDTH}px` }}>
+            <MuiMaterial.DialogTitle id="alert-dialog-title" variant="h5">
+              Bookmark scanning
+            </MuiMaterial.DialogTitle>
+            <MuiMaterial.DialogContent>
+              <Box sx={{ padding: `${MODAL_ROW_PADDING_IN_REM}rem`, display: 'flex' }}>
+                <Typography sx={{ flex: 0 }}>🔖</Typography>
+                <Typography sx={{ paddingLeft: `${MODAL_ROW_PADDING_IN_REM * 5}rem`, flex: 1 }}>
+                  Bookmarks
+                </Typography>
+                <Typography sx={{ flex: 0 }}>12</Typography>
+              </Box>
+              <Box sx={{ padding: `${MODAL_ROW_PADDING_IN_REM}rem`, display: 'flex' }}>
+                <Typography sx={{ flex: 0 }}>📁</Typography>
+                <Typography sx={{ paddingLeft: `${MODAL_ROW_PADDING_IN_REM * 5}rem`, flex: 1 }}>
+                  Folders
+                </Typography>
+                <Typography sx={{ flex: 0 }}>3</Typography>
+              </Box>
+              <Box sx={{ padding: `${MODAL_ROW_PADDING_IN_REM}rem`, display: 'flex' }}>
+                <Typography sx={{ flex: 0 }}>🟢</Typography>
+                <Typography sx={{ paddingLeft: `${MODAL_ROW_PADDING_IN_REM * 5}rem`, flex: 1 }}>
+                  Online
+                </Typography>
+                <Typography sx={{ flex: 0 }}>4</Typography>
+              </Box>
+              <Box sx={{ padding: `${MODAL_ROW_PADDING_IN_REM}rem`, display: 'flex' }}>
+                <Typography sx={{ flex: 0 }}>🟡</Typography>
+                <Typography sx={{ paddingLeft: `${MODAL_ROW_PADDING_IN_REM * 5}rem`, flex: 1 }}>
+                  Time out
+                </Typography>
+                <Typography sx={{ flex: 0 }}>4</Typography>
+              </Box>
+              <Box sx={{ padding: `${MODAL_ROW_PADDING_IN_REM}rem`, display: 'flex' }}>
+                <Typography sx={{ flex: 0 }}>🔴</Typography>
+                <Typography sx={{ paddingLeft: `${MODAL_ROW_PADDING_IN_REM * 5}rem`, flex: 1 }}>
+                  Offline
+                </Typography>
+                <Typography sx={{ flex: 0 }}>4</Typography>
+              </Box>
+              <Box sx={{ padding: `${MODAL_ROW_PADDING_IN_REM}rem`, display: 'flex' }}>
+                <Typography sx={{ flex: 0 }}>⏰</Typography>
+                <Typography sx={{ paddingLeft: `${MODAL_ROW_PADDING_IN_REM * 5}rem`, flex: 1 }}>
+                  Elapsed time
+                </Typography>
+                <Typography sx={{ flex: 0 }}>3s</Typography>
+              </Box>
+              <Box
+                sx={{
+                  padding: `${MODAL_ROW_PADDING_IN_REM * 10}rem 0 ${MODAL_ROW_PADDING_IN_REM}rem ${MODAL_ROW_PADDING_IN_REM}rem`
+                }}
+              >
+                <MuiMaterial.LinearProgress />
+              </Box>
+            </MuiMaterial.DialogContent>
+            <MuiMaterial.DialogActions>
+              <MuiMaterial.Button onClick={handleModalClose}>Stop</MuiMaterial.Button>
+              <MuiMaterial.Button onClick={handleModalClose} autoFocus>
+                Start
+              </MuiMaterial.Button>
+            </MuiMaterial.DialogActions>
+          </Box>
+        </MuiMaterial.Dialog>
+      </Fragment>
     </Box>
   )
 }
