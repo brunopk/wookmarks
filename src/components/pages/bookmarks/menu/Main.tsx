3c2c81feb8daa3e49fc5c2865fae64c1cc32d5aa
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-03-14 16:26:42 -0300

    Improvement to prevent rendering links as folders

--- src/components/pages/bookmarks/menu/Main.tsx
+++ src/components/pages/bookmarks/menu/Main.tsx
@@ -6,22 +6,25 @@ type MenuProps = {
   widthInRem: number
 }
 
-function Main({nodes, widthInRem}: MenuProps) {
+function Main({ nodes, widthInRem }: MenuProps) {
   const [selectedId, setSelectedId] = useState<number>(nodes[0].id)
-    
+
   return (
     <>
-      {nodes.map((node) => (
-        <FolderTree
-          node={node}
-          widthInRem={widthInRem}
-          level={0}
-          selectedId={selectedId}
-          setSelectedId={setSelectedId}
-          key={node.id}
-          parentIsOpen
-        />
-      ))}
+      {nodes.map(
+        (node) =>
+          node.isFolder && (
+            <FolderTree
+              node={node}
+              widthInRem={widthInRem}
+              level={0}
+              selectedId={selectedId}
+              setSelectedId={setSelectedId}
+              key={node.id}
+              parentIsOpen
+            />
+          )
+      )}
     </>
   )
 }
